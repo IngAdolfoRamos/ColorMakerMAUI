e279401caf38deb5d03078279ed088510144879f
adolforamos <ingadolforamos@outlook.com>
2022-09-10 01:21:29 -0500

    Generating random color from button click, avoiding call setcolor event many times from random color by modifying slider color automatically

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -1,7 +1,11 @@
-﻿namespace ColorMaker;
+﻿using System.Diagnostics;
+
+namespace ColorMaker;
 
 public partial class MainPage : ContentPage
 {
+	bool isRandom = false;
+
 	public MainPage()
 	{
 		InitializeComponent();
@@ -9,20 +13,44 @@ public partial class MainPage : ContentPage
 
 	private void ColorValueChangedS(object sender, ValueChangedEventArgs e)
     {
-		var redColor = RedSlider.Value;
-		var blueColor = BlueSlider.Value;
-		var greenColor = GreenSlider.Value;
+        if (!isRandom)
+        {
+            var redColor = RedSlider.Value;
+            var blueColor = BlueSlider.Value;
+            var greenColor = GreenSlider.Value;
 
-		Color color = Color.FromRgb(redColor, greenColor, blueColor);
+            Color color = Color.FromRgb(redColor, greenColor, blueColor);
 
-		SetColor(color);
+            SetColor(color);
+        }
     }
 
 	private void SetColor(Color color)
     {
+		Debug.WriteLine(color.ToString());
 		RandomColorB.BackgroundColor = color;
 		Container.BackgroundColor = color;
 		HexL.Text = color.ToHex();
+    }
+
+	private void GenerateRandomColorB(object sender, EventArgs e)
+    {
+		isRandom = true;
+
+		var random = new Random();
+		var color = Color.FromRgb(
+			random.Next(0, 256),
+			random.Next(0, 256),
+			random.Next(0, 256)
+			);
+
+		SetColor(color);
+
+		RedSlider.Value = color.Red;
+		GreenSlider.Value = color.Green;
+		BlueSlider.Value = color.Blue;
+
+		isRandom = false;
     }
 }
 

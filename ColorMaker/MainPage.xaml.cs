f9522b2933d8977016cd8c12d2eb328ff210b044
adolforamos <ingadolforamos@outlook.com>
2022-09-10 00:52:18 -0500

    Creating RGB color from slider changed values, showing hex color code in label

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -6,6 +6,24 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void ColorValueChangedS(object sender, ValueChangedEventArgs e)
+    {
+		var redColor = RedSlider.Value;
+		var blueColor = BlueSlider.Value;
+		var greenColor = GreenSlider.Value;
+
+		Color color = Color.FromRgb(redColor, greenColor, blueColor);
+
+		SetColor(color);
+    }
+
+	private void SetColor(Color color)
+    {
+		RandomColorB.BackgroundColor = color;
+		Container.BackgroundColor = color;
+		HexL.Text = color.ToHex();
+    }
 }
 
 
